c1f89c4e16b2814369755f21fa0ccc021016b302
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2024-02-09 21:23:29 -0400

    Manejo de argumentos

--- Ejemplo1/Program.cs
+++ Ejemplo1/Program.cs
@@ -10,8 +10,33 @@ namespace Ejemplo1
         ConexionSSL conexion;
         static void Main(string[] args)
         {
-            int num;
+            /*
+             * 1. recibir argumentos
+             * 2. verificar si existe argumento -operacion
+             * 3.  si: evaluar el tipo de operacion
+             *     no: mostrar mensaje de error "Necesita especificar tipo de op"
+             * 4. rescatar valores de -n1 y -n2
+             * 5. operar
+             * 6. mostrar resultado
+             * 
+             * Ejemplo de ejecución:
+             * PM>Ejemplo1 -n1 30 -operación resta -n2 4
+             */
+
+            //Paso 1: Esta en args
+            //Paso 2: iteraciones
+            int longitud = args.Length;// 6
+            //hablar sobre iteraciones (loops)
+
+
+
+            int num, num2;
             num = 45;
+            num2 = 3;
+
+            int sum = num + num2;
+
+            Console.WriteLine("La suma es: " + sum);
 
             float peso = 50.0f;
             double estatura = 1.7565465464;
@@ -19,7 +44,9 @@ namespace Ejemplo1
             float distancia = 50;
             double altura = 40;
             double ancho = 40.5f;
+            Console.WriteLine(altura + ancho);
 
+            Console.WriteLine("La suma es: " + (altura + ancho));
             string usuario;
             usuario = "Pepe";
 
